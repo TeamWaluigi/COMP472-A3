64cffd789305caf27decc5754cdf19d29a531656
Daniel <danielwiktorczyk@gmail.com>
2020-12-06 18:22:20 -0500

    Define Main Script Skeleton and Execution

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("hi")
+print("Main File Start")
+
+print("2.1 INPUT & PARAMETERS")
+
+print("Obtaining Vocabularies")
+print("- Original Vocabulary, with all words, for model NB-BOW-OV")
+print("- Filtered Vocabulary, with only words appearing at least twice, for model NB-BOW-FV")
+
+print("Set additive smoothing add-delta=0.01")
+print("Set space to log base 10 to avoid arithmetic underflow")
+
+
+print("2.2 OUTPUT")
+
+print("Output for NB-BOW-OV")
+print("- Trace")
+print("- Overall Evaluation File")
+
+print("Output for NB-BOW-FV")
+print("- Trace")
+print("- Overall Evaluation File")
+
+
+print("3.0 THE LSTM CLASSIFIER (LSTM-W2V)")
+
+print("Run the LSTM classifier provided by our wonderful TAs to compare performance")
+
+
+
