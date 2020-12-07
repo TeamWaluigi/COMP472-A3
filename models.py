45b94ee1a4616bf8da39f65b996943bdf602f882
Daniel <danielwiktorczyk@gmail.com>
2020-12-07 17:15:46 -0500

    Tally Up Words by Q1 Value
    Co-authored-by: Waqar Qureshi<qureshi.waqarh@yahoo.com>

--- models.py
+++ models.py
@@ -6,7 +6,33 @@ class NaiveBayesClassifier:
         self.space = space
 
     def train(self):
-        pass
+        factual_word_count = dict()
+        non_factual_word_count = dict()
+
+        for index, row in self.training_set.iterrows():
+            text = row["text"]
+            is_factual = row["q1_label"] == "yes"
+            words = text.split(" ")
+
+            for word in words:
+                formatted_word = word.lower()
+
+                if formatted_word not in self.vocabulary:
+                    continue
+                # TODO strip words of things like periods?
+
+                if is_factual:
+                    if formatted_word in factual_word_count:
+                        factual_word_count[formatted_word] += + 1
+                    else:
+                        factual_word_count[formatted_word] = 1
+                else:
+                    if formatted_word in non_factual_word_count:
+                        non_factual_word_count[formatted_word] += + 1
+                    else:
+                        non_factual_word_count[formatted_word] = 1
+
+        print("finished mapping words to class")
 
     def evaluate(self, test_set):
         return self.get_resulting_trace(), self.get_resulting_evaluation()
