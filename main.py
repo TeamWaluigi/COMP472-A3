d8c9f630bcd6156b817eb7c9cbf488e407d154e4
Daniel <danielwiktorczyk@gmail.com>
2020-12-08 11:51:46 -0500

    Clean up the Logs

--- main.py
+++ main.py
@@ -3,30 +3,30 @@ from metrics import output_trace, output_overall_evaluation
 from models import NaiveBayesClassifier
 from vocabulary import extract_vocabulary_original, extract_vocabulary_filtered
 
-print("Main File Start")
+print("\nMain File Start")
 
-print("1 - THE DATASET")
+print("\n\n\n1 - THE DATASET")
 
-print("Loading Datasets")
+print("\nLoading Datasets")
 training_set_data = load_dataset("Input/covid_training.tsv")
 test_set_data = load_dataset("Input/covid_test_public.tsv", False)
 
-print("2 - THE Naive Bayes Classifier (NB-BOW)")
+print("\n\n\n2 - THE Naive Bayes Classifier (NB-BOW)")
 
-print("2.1 PARAMETERS")
+print("\n\n2.1 PARAMETERS")
 
-print("Obtaining Vocabularies")
-print("- Original Vocabulary, with all words, for model NB-BOW-OV")
+print("\nObtaining Vocabularies")
+print("\n- Original Vocabulary, with all words, for model NB-BOW-OV")
 original_vocabulary = extract_vocabulary_original(training_set_data)
 print("- Filtered Vocabulary, with only words appearing at least twice, for model NB-BOW-FV")
 filtered_vocabulary = extract_vocabulary_filtered(training_set_data)
 
-print("Set additive smoothing add-delta=0.01")
+print("\nSet additive smoothing add-delta=0.01")
 add_delta = 0.01
-print("Set space to log base 10 to avoid arithmetic underflow")
+print("\nSet space to log base 10 to avoid arithmetic underflow")
 space = 10
 
-print("Construct our models")
+print("\nConstruct our models")
 nb_bow_ov = NaiveBayesClassifier(training_set=training_set_data,
                                  vocabulary=original_vocabulary,
                                  add_delta=add_delta,
@@ -36,29 +36,29 @@ nb_bow_fv = NaiveBayesClassifier(training_set=training_set_data,
                                  add_delta=add_delta,
                                  space=space)
 
-print("Train our models")
+print("\nTrain our models")
 nb_bow_ov.train()
 nb_bow_fv.train()
 
-print("2.2 OUTPUT")
+print("\n\n2.2 OUTPUT")
 
 output_path = "Output/"
 
-print("Output for NB-BOW-OV")
+print("\nOutput for NB-BOW-OV")
 nb_bow_ov_scores = nb_bow_ov.evaluate(test_set=test_set_data)
 print("- Trace")
 output_trace(test_set_data, nb_bow_ov_scores, output_path + "trace_NB-BOW-OV.txt")
 print("- Overall Evaluation File")
 output_overall_evaluation(test_set_data, nb_bow_ov_scores, output_path + "eval_NB-BOW-OV.txt")
 
-print("Output for NB-BOW-FV")
+print("\nOutput for NB-BOW-FV")
 nb_bow_fv_scores = nb_bow_fv.evaluate(test_set=test_set_data)
 print("- Trace")
 output_trace(test_set_data, nb_bow_ov_scores, output_path + "trace_NB-BOW-FV.txt")
 print("- Overall Evaluation File")
 output_overall_evaluation(test_set_data, nb_bow_fv_scores, output_path + "eval_NB-BOW-FV.txt")
 
-print("3 - THE LSTM CLASSIFIER (LSTM-W2V)")
+print("\n\n\n3 - THE LSTM CLASSIFIER (LSTM-W2V)")
 
 print("Run the LSTM classifier provided by our wonderful TAs to compare performance")
 print("- Code downloaded from https://gitlab.com/Feasinde/lstm-for-covid-disinformation")
